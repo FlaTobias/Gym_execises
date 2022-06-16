7d63ec4a8fbc2a9c87451860d6ed35686fa01418
Flavio Tobias <flavioa.tobias@gmail.com>
2022-06-15 23:49:10 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 import './App.css';
+import { Home } from '@mui/icons-material';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={ <ExerciseDetail />} />
+      </Routes>
+    </Box>
   )
 }
 
