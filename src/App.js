81b2cbd3288f317bf402071ea2a913b7851e22f8
Flavio Tobias <flavioa.tobias@gmail.com>
2022-06-15 23:55:58 -0300

    Footer Routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
-import { Home } from '@mui/icons-material';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -13,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={ <ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   )
 }
